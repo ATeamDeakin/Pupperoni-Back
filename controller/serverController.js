bcf75bb2e4cb970c93414d202dc590d5feb88a72
akshatarora02 <akshatarora028@gmail.com>
2022-09-20 22:39:15 +1000

    Added MVC framework structure, created and validated schema

---
+++ controller/serverController.js
@@ -0,0 +1,10 @@
+const User = require("../models/User")
+
+require("dotenv").config()
+
+const port = process.env.TOKEN_SERVER_PORT
+
+
+// module.exports = {
+    
+// }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
+const User = require("./models/User")
 
 require('dotenv').config();
 
